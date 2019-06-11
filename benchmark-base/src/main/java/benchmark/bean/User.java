6e355662c2251c18f92316617ccf461bed318c0f
ken.lj <ken.lj.hz@gmail.com>
2019-06-11 13:23:23 +0800

    Revert "date"
    This reverts commit 74f69d95

--- benchmark-base/src/main/java/benchmark/bean/User.java
+++ benchmark-base/src/main/java/benchmark/bean/User.java
@@ -1,127 +1,126 @@
 package benchmark.bean;
 
 import java.io.Serializable;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 public class User implements Serializable {
-    private static final long serialVersionUID = 2566816725396650300L;
+	private static final long serialVersionUID = 2566816725396650300L;
 
-    private long id;
-    private String name;
-    private int sex;
-    private LocalDate birthday;
-    private String email;
-    private String mobile;
-    private String address;
-    private String icon;
-    private List<Integer> permissions;
-    private int status;
-    private LocalDateTime createTime;
-    private LocalDateTime updateTime;
+	private long id;
+	private String name;
+	private int sex;
+	private Date birthday;
+	private String email;
+	private String mobile;
+	private String address;
+	private String icon;
+	private List<Integer> permissions;
+	private int status;
+	private Date createTime;
+	private Date updateTime;
 
-    public long getId() {
-        return id;
-    }
+	public long getId() {
+		return id;
+	}
 
-    public void setId(long id) {
-        this.id = id;
-    }
+	public void setId(long id) {
+		this.id = id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public int getSex() {
-        return sex;
-    }
+	public int getSex() {
+		return sex;
+	}
 
-    public void setSex(int sex) {
-        this.sex = sex;
-    }
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
 
-    public LocalDate getBirthday() {
-        return birthday;
-    }
+	public Date getBirthday() {
+		return birthday;
+	}
 
-    public void setBirthday(LocalDate birthday) {
-        this.birthday = birthday;
-    }
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public String getMobile() {
-        return mobile;
-    }
+	public String getMobile() {
+		return mobile;
+	}
 
-    public void setMobile(String mobile) {
-        this.mobile = mobile;
-    }
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
 
-    public String getAddress() {
-        return address;
-    }
+	public String getAddress() {
+		return address;
+	}
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
+	public void setAddress(String address) {
+		this.address = address;
+	}
 
-    public String getIcon() {
-        return icon;
-    }
+	public String getIcon() {
+		return icon;
+	}
 
-    public void setIcon(String icon) {
-        this.icon = icon;
-    }
+	public void setIcon(String icon) {
+		this.icon = icon;
+	}
 
-    public List<Integer> getPermissions() {
-        return permissions;
-    }
+	public List<Integer> getPermissions() {
+		return permissions;
+	}
 
-    public void setPermissions(List<Integer> permissions) {
-        this.permissions = permissions;
-    }
+	public void setPermissions(List<Integer> permissions) {
+		this.permissions = permissions;
+	}
 
-    public int getStatus() {
-        return status;
-    }
+	public int getStatus() {
+		return status;
+	}
 
-    public void setStatus(int status) {
-        this.status = status;
-    }
+	public void setStatus(int status) {
+		this.status = status;
+	}
 
-    public LocalDateTime getCreateTime() {
-        return createTime;
-    }
+	public Date getCreateTime() {
+		return createTime;
+	}
 
-    public void setCreateTime(LocalDateTime createTime) {
-        this.createTime = createTime;
-    }
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
 
-    public LocalDateTime getUpdateTime() {
-        return updateTime;
-    }
+	public Date getUpdateTime() {
+		return updateTime;
+	}
 
-    public void setUpdateTime(LocalDateTime updateTime) {
-        this.updateTime = updateTime;
-    }
+	public void setUpdateTime(Date updateTime) {
+		this.updateTime = updateTime;
+	}
 
-    @Override
-    public String toString() {
-        return "User [id=" + id + ", name=" + name + ", sex=" + sex + ", birthday=" + birthday + ", email=" + email
-                + ", mobile=" + mobile + ", address=" + address + ", icon=" + icon + ", permissions=" + permissions
-                + ", status=" + status + ", createTime=" + createTime + ", updateTime=" + updateTime + "]";
-    }
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", sex=" + sex + ", birthday=" + birthday + ", email=" + email
+				+ ", mobile=" + mobile + ", address=" + address + ", icon=" + icon + ", permissions=" + permissions
+				+ ", status=" + status + ", createTime=" + createTime + ", updateTime=" + updateTime + "]";
+	}
 
 }
